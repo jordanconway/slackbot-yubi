6a06f9c716183bb1edd3aebc53a4043af59d8840
Jordan Conway <jconway@linuxfoundation.org>
2015-10-15 16:18:17 -0400

    Fixed api_token lookup, moved to random yubisays strings from config file

--- slack_yubi.py
+++ slack_yubi.py
@@ -2,13 +2,23 @@
 import sys
 import time
 import yaml
+import random
 from slackclient import SlackClient
 
+# Get the config file
 with open('config.yml','r') as ymlfile:
   cfg = yaml.load(ymlfile)
+
+# Set some variables from the config file
 yubistring = cfg["yubikey"]["yubistring"]
-token = cfg["slack"]["apitoken"] 
+token = cfg["slack"]["api_token"]
+yubisays = cfg["yubisays"]
 sc = SlackClient(token)
+
+# Chomp yubistring to first 6 chars
+if len(yubistring) > 6:
+   yubistring = yubistring[:6]
+
 if sc.rtm_connect():
   while True:
     new_evts =  sc.rtm_read()
@@ -18,7 +28,7 @@ if sc.rtm_connect():
             message=evt["text"]
             pline=""
             if yubistring in message and len(message) == 44:
-              pline="Hi yubi!"
+              pline=random.choice (yubisays)
               chan = sc.server.channels.find(evt["channel"])
               if chan:
                 chan.send_message(pline)
